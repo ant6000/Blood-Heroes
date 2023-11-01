cc3c806495228682ef72dc2057c2d45d285f71f5
antor <antorofficial20@gmail.com>
2023-11-01 10:40:44 +0600

    logo added

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:blood_heroes/view/screens/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 800),
+      minTextAdapt: true,
+      builder: (_,child) {
+        return MaterialApp(
+          title: 'Blood Fighter',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+            useMaterial3: true,
+          ),
+          initialRoute: '/',
+          routes: {
+            '/':(context) =>  LoginPage(),
+          },
+        );
+      }
+    );
+  }
+}
+
---
+++ lib/view/screens/login_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LoginPage extends StatelessWidget {
+   LoginPage({super.key});
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool value = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red.shade900,
+        title: const Text('Blood Fighter',
+            style: TextStyle(fontSize: 20, color: Colors.white)),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('/images/save_life_logo.png'),
+            SizedBox(
+              height: 10.h,
+            ),
+            TextField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.mail),
+                  label: const Text('Enter your email'),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20))),
+            ),
+            SizedBox(height: 10.h),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              keyboardType: TextInputType.visiblePassword,
+              decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.password),
+                  suffixIcon: GestureDetector(
+                      onTap: () {}, child: const Icon(Icons.remove_red_eye)),
+                  label: const Text('Enter your password'),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20))),
+            ),
+            SizedBox(height: 10.h),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(onPressed: () {}, child: const Text('Forgor Password')),
+                Row(
+                  children: [
+                    Checkbox(value: value, onChanged: (value) {
+                    },),
+                    SizedBox(width: 5.w),
+                    const Text('Remember me'),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 10.h),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.red, borderRadius: BorderRadius.circular(20)),
+                child: const Center(
+                  child: Text('Login',
+                      style: TextStyle(fontSize: 30, color: Colors.white)),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
